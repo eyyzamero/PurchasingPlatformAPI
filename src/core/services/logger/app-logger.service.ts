3835f7fa67ed6b8c20efdb86bfa9c89c5b236afe
eyyzamero <eazymen789@gmail.com>
2023-01-09 21:00:32 +0100

    Added log level methods to AppLoggerService

--- src/core/services/logger/app-logger.service.ts
+++ src/core/services/logger/app-logger.service.ts
@@ -9,7 +9,19 @@ export class AppLoggerService {
     private _context: AppContext = AppContext.NONE
   ) { }
 
+  debug(message: string): void {
+    console.log(`${ConsoleColor.BACKGROUND_MAGENTA}[DEBUG]${ConsoleStyle.RESET} ${message}`);
+  }
+
   info(message: string): void {
-    console.log(`${ConsoleColor.FOREGROUND_CYAN}[INFO]${ConsoleStyle.RESET} - ${message}`);
+    console.log(`${ConsoleColor.FOREGROUND_CYAN}[INFO]${ConsoleStyle.RESET} ${message}`);
+  }
+
+  danger(message: string): void {
+    console.log(`${ConsoleColor.FOREGROUND_YELLOW}[DANGER]${ConsoleStyle.RESET} ${message}`);
+  }
+
+  error(message: string): void {
+    console.log(`${ConsoleColor.FOREGROUND_RED}[ERROR]${ConsoleStyle.RESET} ${message}`);
   }
 }
